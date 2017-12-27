d20d5ec66cf023238428f6b968b1282cae560edb
koszany <agnieszka.koszany@codecool.com>
2017-12-27 11:01:55 +0100

    Create comprehension.py

---
+++ comprehension.py
@@ -0,0 +1,33 @@
+import random
+
+guesses_taken = 0
+
+print('Hello! What is your name?')
+myName = input()
+
+number = random.randint(1, 20)
+print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
+
+while guesses_taken < 6:
+    print('Take a guess.')
+    guess = input()
+    guess = int(guess)
+
+    guesses_taken += 1
+
+    if guess < number:
+        print('Your guess is too low.')
+
+    if guess > number:
+        print('Your guess is too high.')
+
+    if guess == number:
+        break
+
+if guess == number:
+    guesses_taken = str(guesses_taken)
+    print('Good job, ' + myName + '! You guessed my number in ' + guesses_taken + ' guesses!')
+
+if guess != number:
+    number = str(number)
+    print('Nope. The number I was thinking of was ' + number)
